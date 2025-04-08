7e9c8a472e1ba69fb15bbf2ac61a56802df0f578
shreyal-sharma <ss856@njit.edu>
2025-04-08 10:16:34 -0400

    Added new layout for the Challenge section UI

--- app/app/src/main/java/com/example/neversnooze/MainActivity.kt
+++ app/app/src/main/java/com/example/neversnooze/MainActivity.kt
@@ -16,32 +16,9 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Sample data
-        val sampleAlarms = listOf(
-            Alarm("9:27 AM", "Never Snooze", "Daily"),
-            Alarm("10:00 AM", "Class", "Weekdays"),
-            Alarm("7:00 PM", "Throw out Trash", "Daily"),
-            Alarm("11:30 PM", "Sleep", "Daily")
-        )
+
 
         // Setup RecyclerView
-        val recyclerView = findViewById<RecyclerView>(R.id.alarmRecyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = AlarmAdapter(sampleAlarms)
-
-        // Add divider decoration to RecyclerView
-        val divider = DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        ContextCompat.getDrawable(this, R.drawable.divider)?.let { drawable ->
-            divider.setDrawable(drawable)
-        }
-        recyclerView.addItemDecoration(divider)
-
-        // Change R.id.main to your root ConstraintLayout's ID
-        // Edge-to-edge handling
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.buttonBar)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
-}
+
